a27397d458ff84fc829b896d65c16f10ade122a2
CHAUVIRÉ Romain <rom1cha44@gmail.com>
2018-01-22 12:54:15 +0100

    tuto

--- public/javascript/client.js
+++ public/javascript/client.js
@@ -1,3 +1,18 @@
  $(document).ready(function() {
+	 var name = '';
+	 function go() {
+		 name = $('#user-name').val();
+		 $('#user-name').val('');
+		 $('.user-form').hide();
+		 $('.chat-box').show();
+	 };
 
+	 $('#user-name').keydown(function(e) {
+		 if(e.keyCode == 13){ //Enter pressed
+			 go();
+		 }
+	 });
+	 $('.go-user').on('click', function(e) {
+		 go();
+	 });
  });
