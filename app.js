3a9fe75db03845b32b5411ea05d9c2309d598a57
CHAUVIRÉ Romain <rom1cha44@gmail.com>
2018-01-29 12:13:28 +0100

    online functionement

--- app.js
+++ app.js
@@ -18,3 +18,21 @@ app.get('/', function(req, res) {
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+var clients = [];
+// Poll endpoint
+app.get('/poll/*', function(req, res) {
+	clients.push(res);
+});
+
+// Msg endpoint
+app.post('/msg', function(req, res) {
+	message = req.body;
+	var msg = JSON.stringify(message);
+	while(clients.length > 0) {
+		var client = clients.pop();
+		client.end(msg);
+	}
+	res.end();
+});
+
