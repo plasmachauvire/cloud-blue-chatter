21a6ed09b0755c33f53381b929ad0c466ca936cd
CHAUVIRÉ Romain <rom1cha44@gmail.com>
2018-01-29 12:00:40 +0100

    fix button go

--- public/javascript/client.js
+++ public/javascript/client.js
@@ -1,4 +1,11 @@
  $(document).ready(function() {
+	 var name = '';
+	 function go() {
+		 name = $('#user-name').val();
+		 $('#user-name').val('');
+		 $('.user-form').hide();
+		 $('.chat-box').show();
+	 };
 
 	 $('#user-name').keydown(function(e) {
 		 if(e.keyCode == 13){ //Enter pressed
@@ -9,11 +16,3 @@
 		 go();
 	 });
  });
-
- var name = '';
- function go() {
-	 name = $('#user-name').val();
-	 $('#user-name').val('');
-	 $('.user-form').hide();
-	 $('.chat-box').show();
- };
