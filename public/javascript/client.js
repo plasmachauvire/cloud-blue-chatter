df1336d871155c72f97fd6dd7008562543843aad
CHAUVIRÉ Romain <rom1cha44@gmail.com>
2018-01-29 11:56:29 +0100

    go button

--- public/javascript/client.js
+++ public/javascript/client.js
@@ -1,11 +1,4 @@
  $(document).ready(function() {
-	 var name = '';
-	 function go() {
-		 name = $('#user-name').val();
-		 $('#user-name').val('');
-		 $('.user-form').hide();
-		 $('.chat-box').show();
-	 };
 
 	 $('#user-name').keydown(function(e) {
 		 if(e.keyCode == 13){ //Enter pressed
@@ -16,3 +9,11 @@
 		 go();
 	 });
  });
+
+ var name = '';
+ function go() {
+	 name = $('#user-name').val();
+	 $('#user-name').val('');
+	 $('.user-form').hide();
+	 $('.chat-box').show();
+ };
